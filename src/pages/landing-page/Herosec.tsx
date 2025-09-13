104668dbffd2d4f49fe285a18b7b92a91be49e38
Quincy Walker <qwalker@nvidia.com>
2025-09-13 09:45:46 -0400

    updated site

--- src/pages/landing-page/Herosec.tsx
+++ src/pages/landing-page/Herosec.tsx
@@ -42,9 +42,9 @@ const Herosec: React.FC = () => {
                   className="m-0 font-medium text-3xl text-white"
                   style={{ lineHeight: "40px" }}
                 >
-                  International
+                  The only 
                   <span className="themeClr"> cash application </span>
-                  to accept credit and online payments!
+                  you need to accept credit and online payments!
                 </h2>
                 <div className="btnWrpper mt-3 flex items-center gap-2 flex-wrap">
                   <Link
@@ -76,7 +76,7 @@ const Herosec: React.FC = () => {
                       style={{ height: 15 }}
                     />
                   </b>{" "}
-                  The app to get money for international gig workers.
+                  The app to get money for international remittance.
                   Get paid fast with our international money transfer service.
                   Receive payments from over 45 different countries.
                   Cash out your online payments to a bank or mobile money. 
