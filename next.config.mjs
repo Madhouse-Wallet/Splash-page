e8eb4dd066211117904c34ccafefd0b648c2aac0
Quincy Walker <qwalker@nvidia.com>
2025-09-23 20:09:08 -0400

    adding security headers

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,18 @@ const nextConfig = {
         source: '/:path*',
         headers: [
           { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'Content-Security-Policy', value: "frame-ancestors 'none'" },
+            {
+              key: 'X-XSS-Protection',
+              value: '1; mode=block',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+        // {
+        //   key: 'Content-Security-Policy',
+        //   value: "default-src 'self'; object-src 'none'; frame-ancestors 'none';",
+        // },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' }
