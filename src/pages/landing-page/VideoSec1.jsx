4a2ab50315a337489b55ae68b2a4b0f04b7b70d3
Quincy Walker <qwalker@nvidia.com>
2025-12-14 23:35:26 +0300

    updated pics and vids

--- src/pages/landing-page/VideoSec1.jsx
+++ src/pages/landing-page/VideoSec1.jsx
@@ -30,7 +30,7 @@ const VideoSec1 = () => {
                 playsInline
                 preload="none"
               >
-                <source src="./tpos.mp4" />
+                <source src="./tpos.mov" />
               </video>
             </div>
           </div>
